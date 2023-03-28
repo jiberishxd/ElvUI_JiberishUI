ed004795c0b1510eaa7001a228ee76b2182f1fff
jiberishxd <129198710+jiberishxd@users.noreply.github.com>
2023-03-28 12:56:12 -0600

    deleted line after credits
    included "Tukui/discord community" in same line as VXT/fab others

--- ElvUI_JiberishUI/Init.lua
+++ ElvUI_JiberishUI/Init.lua
@@ -28,8 +28,7 @@ local JiberishUI = E:NewModule(MyPluginName, "AceHook-3.0", "AceEvent-3.0", "Ace
 local CREDITS = {
 	"|cff0DB1D0J|r|cff18A2D2i|r|cff2494D4b|r|cff2F86D7e|r|cff3B78D9r|r|cff4669DBi|r|cff525BDEs|r|cff5D4DE0h|r - Author",
 	'|cff82B4ffEltreum|r - Programming, Inspiration, LEGEND!',
-	'Vxt, Fab, Pasch, Trenchy, Blinkii, Merathilis, Benik, and so many others ',
-	'Tukui.org and the Elvui Share Your UI community Discord',
+	'Vxt, Fab, Pasch, Trenchy, Blinkii, Merathilis, Benik, Tukui.org, and the amazing ElvUI Discord community! ',
 }
 local CREDITS_STRING = tconcat(CREDITS, '|n')
 
