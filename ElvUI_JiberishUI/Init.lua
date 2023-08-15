8e73b2ba09354f9e2115824ba303d4d023f74930
Eltreum <30246110+eltreum0@users.noreply.github.com>
2023-08-15 14:06:48 -0300

    update credits

--- ElvUI_JiberishUI/Init.lua
+++ ElvUI_JiberishUI/Init.lua
@@ -27,10 +27,11 @@ local JiberishUI = E:NewModule(MyPluginName, "AceHook-3.0", "AceEvent-3.0", "Ace
 
 -- example of credits if you want to add some
 local CREDITS = {
-	"|cff0DB1D0J|r|cff18A2D2i|r|cff2494D4b|r|cff2F86D7e|r|cff3B78D9r|r|cff4669DBi|r|cff525BDEs|r|cff5D4DE0h|r - Author",
-	'|cff82B4ffEltreum|r - Programming, Inspiration, LEGEND!',
-	'Vxt, Fab, Pasch, Trenchy, Blinkii, Merathilis, Benik, Tukui.org, and the amazing ElvUI Discord community! ',
-}
+    "|cff0DB1D0J|r|cff18A2D2i|r|cff2494D4b|r|cff2F86D7e|r|cff3B78D9r|r|cff4669DBi|r|cff525BDEs|r|cff5D4DE0h|r - Author",
+    '|cff82B4ffEltreum|r - Programming, Inspiration, LEGEND!',
+    'Vxt, Fab, Pasch, Trenchy, Blinkii, Merathilis, Benik, Tukui.org, and the amazing ElvUI Discord community! ',
+    'FABLED MYTHS thank you for your massive support! - Butze, Mörf ',
+  }
 local CREDITS_STRING = tconcat(CREDITS, '|n')
 
 --This function will hold your layout settings
