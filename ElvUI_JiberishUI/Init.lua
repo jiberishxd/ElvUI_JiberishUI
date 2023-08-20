405be0a82f8f6a6a4ec956152b5e4a57bab92f78
Eltreum <30246110+eltreum0@users.noreply.github.com>
2023-08-20 17:51:42 -0300

    add immortal myths

--- ElvUI_JiberishUI/Init.lua
+++ ElvUI_JiberishUI/Init.lua
@@ -29,8 +29,9 @@ local JiberishUI = E:NewModule(MyPluginName, "AceHook-3.0", "AceEvent-3.0", "Ace
 local CREDITS = {
     "|cff0DB1D0J|r|cff18A2D2i|r|cff2494D4b|r|cff2F86D7e|r|cff3B78D9r|r|cff4669DBi|r|cff525BDEs|r|cff5D4DE0h|r - Author",
     '|cff82B4ffEltreum|r - Programming, Inspiration, LEGEND!',
-    'Vxt, Fab, Pasch, Trenchy, Blinkii, Merathilis, Benik, Tukui.org, and the amazing ElvUI Discord community! ',
-    'FABLED MYTHS thank you for your massive support! - Butze, Mörf ',
+    'Vxt, Fab, Pasch, Trenchy, Blinkii, Merathilis, Benik, Tukui.org, and the amazing ElvUI Discord community!',
+    'FABLED MYTHS thank you for your massive support! - Butze, Mörf',
+    "IMMORTAL MYTHS - Seba and OrificeHorace",
   }
 local CREDITS_STRING = tconcat(CREDITS, '|n')
 
