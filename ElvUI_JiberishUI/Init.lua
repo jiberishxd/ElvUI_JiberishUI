23679731a2ed529c3b2bc3256e983185562d1080
jiberishxd <129198710+jiberishxd@users.noreply.github.com>
2023-08-29 12:05:25 -0600

    Update Init.lua

--- ElvUI_JiberishUI/Init.lua
+++ ElvUI_JiberishUI/Init.lua
@@ -30,8 +30,10 @@ local CREDITS = {
     "|cff0DB1D0J|r|cff18A2D2i|r|cff2494D4b|r|cff2F86D7e|r|cff3B78D9r|r|cff4669DBi|r|cff525BDEs|r|cff5D4DE0h|r - Author",
     '|cff82B4ffEltreum|r - Programming, Inspiration, LEGEND!',
     'Vxt, Fab, Pasch, Trenchy, Blinkii, Merathilis, Benik, Tukui.org, and the amazing ElvUI Discord community!',
-    'FABLED MYTHS thank you for your massive support! - Butze, Mörf',
-    "IMMORTAL MYTHS - Seba and OrificeHorace",
+    'FABLED CHAMPIONS thank you for your support! - Exs, Kiyong',
+	'FABLED MYTHS - Spectated, Mörf, JustKali, MontegoSlay',
+    "FABLED LEGENDS - Butze",
+	"IMMORTAL MYTHS - Seba and OrificeHorace",
   }
 local CREDITS_STRING = tconcat(CREDITS, '|n')
 
