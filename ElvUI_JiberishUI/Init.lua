49c32e18624303b04ff3264ce63bca0cc6c7b5aa
jiberishxd <129198710+jiberishxd@users.noreply.github.com>
2023-09-05 09:58:25 -0600

    Blinkii/Trenchy name positioned first

--- ElvUI_JiberishUI/Init.lua
+++ ElvUI_JiberishUI/Init.lua
@@ -29,7 +29,7 @@ local JiberishUI = E:NewModule(MyPluginName, "AceHook-3.0", "AceEvent-3.0", "Ace
 local CREDITS = {
     "|cff0DB1D0J|r|cff18A2D2i|r|cff2494D4b|r|cff2F86D7e|r|cff3B78D9r|r|cff4669DBi|r|cff525BDEs|r|cff5D4DE0h|r - Author",
     '|cff82B4ffEltreum|r - Programming, Inspiration, LEGEND!',
-    'Vxt, Fab, Pasch, |cffec1000Trenchy|r, |cffff0066Blinkii|r, Merathilis, Benik, Tukui.org, and the amazing ElvUI Discord community!',
+    '|cffec1000Trenchy|r, |cff00baffBlinkii|r, Vxt, Fab, Pasch, Merathilis, Benik, Tukui.org, and the amazing ElvUI Discord community!',
     '|cff00fcdcFABLED CHAMPIONS|r - thank you for your support! - Exs, Kiyong',
 	'|cfff0066FFABLED MYTHS|r - Claq, Spectated, Mörf, JustKali, MontegoSlay, JaxDewars, Thirion',
     "|cff0086ffFABLED LEGENDS|r - Butze",
