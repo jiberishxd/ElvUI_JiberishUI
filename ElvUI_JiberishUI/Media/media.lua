049010fa05b5fc3e960e75c168468ecb0d1a1203
Eltreum <30246110+eltreum0@users.noreply.github.com>
2023-09-04 23:28:18 -0300

    fix name

--- ElvUI_JiberishUI/Media/media.lua
+++ ElvUI_JiberishUI/Media/media.lua
@@ -3,4 +3,4 @@ local LSM = E.Libs.LSM
 
 --Fonts
 LSM:Register("font", "KMT-GothamXN_Ultra", [[Interface\Addons\ElvUI_JiberishUI\Media\Fonts\KMT-GothamXN_Ultra.ttf]])
-LSM:Register("font", "KMT-GothamXN_Ultra", [[Interface\Addons\ElvUI_JiberishUI\Media\Fonts\Poppins-Black.ttf]])
+LSM:Register("font", "Poppins-Black", [[Interface\Addons\ElvUI_JiberishUI\Media\Fonts\Poppins-Black.ttf]])
